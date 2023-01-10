70b4c6da36306d6151b65bc2c1a92e3831fa76de
KarVarr <megatr9n@gmail.com>
2023-01-10 19:23:41 +0300

    added shadow to button and view

--- MagicBallSwift/ViewController.swift
+++ MagicBallSwift/ViewController.swift
@@ -20,8 +20,33 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        createButtonUI()
+        createViewUI()
+       
+        
     }
+    
+    //MARK: - button UI
+    func createButtonUI() {
+        clickButton.layer.cornerRadius = 45
+        clickButton.layer.shadowRadius = 3.0
+        clickButton.layer.shadowColor = UIColor.black.cgColor
+        clickButton.layer.shadowOffset = CGSize(width: 5.0, height: 5.0)
+        clickButton.layer.shadowOpacity = 0.4
+        clickButton.layer.masksToBounds = false
+    }
+    
+    func createViewUI() {
+        textView.layer.cornerRadius = textView.bounds.size.width / 2
+        textView.layer.shadowColor = UIColor.black.cgColor
+        textView.layer.shadowOpacity = 0.3
+        textView.layer.masksToBounds = false
+        textView.layer.shadowOffset = CGSize(width: -5.0, height: -5.0)
+    }
+    
+    
     
     
     
