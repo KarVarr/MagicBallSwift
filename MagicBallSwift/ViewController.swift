29d440f05f216a2a6b9bbcdfafc3bedae8620426
KarVarr <megatr9n@gmail.com>
2023-01-10 18:55:41 +0300

    Initial Commit

---
+++ MagicBallSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicBallSwift
+//
+//  Created by Karen Vardanian on 10.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
