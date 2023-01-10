bc66a5c0a1de501c6d830433fbbae710311168a5
KarVarr <megatr9n@gmail.com>
2023-01-10 19:07:43 +0300

    added button view and label. And color!

--- MagicBallSwift/ViewController.swift
+++ MagicBallSwift/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBOutlet weak var textView: UIView!
+    
+    @IBOutlet weak var clickButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    
+    @IBAction func clickButtonAction(_ sender: UIButton) {
+    }
+    
 
 }
 
