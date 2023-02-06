09acad82300cb1d5029f9dbc68b326a10d1e67eb
KarVarr <megatr9n@gmail.com>
2023-02-06 22:33:35 +0300

    added colors for title and some mark

--- MagicBallSwift/Helpers/Resources.swift
+++ MagicBallSwift/Helpers/Resources.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 enum R {
+    //MARK: - fonts
     enum Fonts {
         static func AppleSDGothicNeoLight (with size: CGFloat) -> UIFont {
             UIFont(name: "AppleSDGothicNeo-Light", size: size) ?? UIFont()
@@ -19,6 +20,15 @@ enum R {
         static let customButtonTitlePress = NSMutableAttributedString(string: "...", attributes: [
             NSAttributedString.Key.font: UIFont(name: "AppleSDGothicNeo-Light", size: 44) ?? UIFont(),
         ])
+    }
+    //MARK: - strings
+    enum Strings {
+        static let askAgain = "Ask me again!"
+    }
+    //MARK: - colors
+    enum Colors {
+        static let black = UIColor.black
+        static let pinkCustom = UIColor(red: 0.84, green: 0.51, blue: 1.00, alpha: 1.00)
     }
     
 }
--- MagicBallSwift/ViewController.swift
+++ MagicBallSwift/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var textView: UIView!
     @IBOutlet weak var clickButton: UIButton!
     
-   
+   let modelView = ModelView()
     
     
     override func viewDidLoad() {
@@ -23,29 +23,7 @@ class ViewController: UIViewController {
         createViewUI()
 
     }
-    //MARK: - Array of random text
-    let textArray = [
-     "YES",
-     "NO",
-     "Sure",
-     "Yup",
-     "Try it tomorrow",
-     "It is certain",
-     "It is decidedly so",
-     "Without a doubt",
-     "Yes, definitely",
-     "You may rely on it",
-     "As I see it, yes",
-     "Signs point to yes",
-     "Ask again later",
-     "Cannot predict now",
-     "My reply is no",
-     "My sources say no",
-     "Most likely",
-     "Very doubtful",
-     "Certainly not",
-     "Never",
-   ];
+    
     
     
     //MARK: - button UI
@@ -68,7 +46,6 @@ class ViewController: UIViewController {
         textView.layer.shadowOffset = (CGSize(width: -15, height: -7))
         textView.layer.shadowOpacity = 0.15
         textView.layer.shadowRadius = 20
-        //UIColor(red: 0.60, green: 0.87, blue: 0.79, alpha: 1.00)
     }
     
     
@@ -76,11 +53,13 @@ class ViewController: UIViewController {
    
     //MARK: - Button Action
     @IBAction func clickButtonAction(_ sender: UIButton) {
-        textLabel.text = textArray.randomElement()
+        textLabel.text = modelView.textArray.randomElement()
+        textLabel.textColor = R.Colors.pinkCustom
+        
         clickButton.isEnabled = false
         clickButton.titleLabel?.font = R.Fonts.AppleSDGothicNeoLight(with: 44)
-//        clickButton.setTitle("...", for: .disabled)
         clickButton.setAttributedTitle(R.Fonts.customButtonTitlePress, for: .normal)
+        
         Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(ViewController.enableButton), userInfo: nil, repeats: false)
         
     }
@@ -88,6 +67,9 @@ class ViewController: UIViewController {
     @objc func enableButton() {
         clickButton.isEnabled = true
         clickButton.setAttributedTitle(R.Fonts.customButtonTitle, for: .normal)
+        
+        textLabel.text = R.Strings.askAgain
+        textLabel.textColor = R.Colors.black
     }
   
    
